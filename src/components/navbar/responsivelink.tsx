ed980e2d5314d434c4c3d3bbea855d4b664dbfe0
ErtugrulArae <aracertugrul407@gmail.com>
2025-04-24 17:48:23 +0300

    sonrütuş

--- src/components/navbar/responsivelink.tsx
+++ src/components/navbar/responsivelink.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { RiHome5Line } from "react-icons/ri";
-import { CiBoxList } from "react-icons/ci";
-import { RiContactsLine } from "react-icons/ri";
-import { MdOutlineRestaurantMenu } from "react-icons/md";
-import { FaRegComment } from "react-icons/fa6";
+import { BsShopWindow } from "react-icons/bs";
+import { GiFly } from "react-icons/gi";
+import { MdBalcony } from "react-icons/md";
 
 
 
@@ -14,12 +13,12 @@ const responsivelink = () => {
                 <RiHome5Line size={30} /> Anasayfa
             </a>
             <a title='Asde yapı Pvc Sistemleri' className='flex  items-center gap-6 text-lg font-medium opacity-90' href="https://www.asdeyapi.com/#pvc">
-                <CiBoxList size={30} />Pvc Kapı Pencere 
+                <BsShopWindow size={30} />Pvc Kapı Pencere 
             </a> <a title='Asde Yapı Sineklik Sistemleri' className='flex  items-center gap-6 text-lg font-medium opacity-90' href="https://www.asdeyapi.com/#sineklik">
-                <RiContactsLine size={30} /> Sineklik
+                <GiFly size={30} /> Sineklik
             </a> 
              <a title='Asde Yapı Cambalkon Sistemleri' className='flex  items-center gap-6 text-lg font-medium opacity-90' href="https://www.asdeyapi.com/#cambalkon">
-                <FaRegComment size={30} /> Cambalkon
+                <MdBalcony size={30} /> Cambalkon
             </a>
 
         </div>
